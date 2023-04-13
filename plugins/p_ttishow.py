4f8e9f603ba21b1bc33e0603a8d3f38e8182c18f
Shibily X Rogue <88287526+Hackermanker@users.noreply.github.com>
2023-04-13 17:18:13 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -37,8 +37,8 @@ async def save_group(bot, message):
             await bot.leave_chat(message.chat.id)
             return
         buttons = [[
-            InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/Oriksonic"),
-            InlineKeyboardButton('🧑‍🏫Tutorial', url='https://t.me/Astra_botz')
+            InlineKeyboardButton(' Help', url=f"https://t.me/botsupportastra"),
+            InlineKeyboardButton('Updates', url='https://t.me/Astra_botz')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
